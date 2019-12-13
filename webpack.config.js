8044b190ae2b847bc4ca000067cc38feca2ba843
JimmieZhou <jimmiezhou@126.com>
2019-12-13 15:26:52 +0800

    mobx-项目框架搭建

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@
  * @Author: jimmiezhou
  * @Date: 2019-12-13 14:37:53
  * @LastEditors: jimmiezhou
- * @LastEditTime: 2019-12-13 14:48:25
+ * @LastEditTime: 2019-12-13 15:26:35
  */
 const path = require('path')
 module.exports = {
@@ -21,7 +21,8 @@ module.exports = {
             use: {
                 loader: 'babel-loader',
                 options: {
-                    presets: ['env']
+                    presets: ['env'],
+                    plugins:['transform-class-properties']
                 }
             }
         }]

3588f10f93829a468d405f73efa2814ed5708846
JimmieZhou <jimmiezhou@126.com>
2019-12-13 15:23:26 +0800

    mobx-项目框架搭建

---
+++ src/index.js
@@ -0,0 +1,9 @@
+/*
+ * @Descripttion: 
+ * @version: 1.0.0
+ * @Author: jimmiezhou
+ * @Date: 2019-12-13 14:35:42
+ * @LastEditors: jimmiezhou
+ * @LastEditTime: 2019-12-13 14:36:44
+ */
+document.write('hello world')
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+/*
+ * @Descripttion: 
+ * @version: 1.0.0
+ * @Author: jimmiezhou
+ * @Date: 2019-12-13 14:37:53
+ * @LastEditors: jimmiezhou
+ * @LastEditTime: 2019-12-13 14:48:25
+ */
+const path = require('path')
+module.exports = {
+    mode: 'development',
+    entry: path.resolve(__dirname, 'src/index.js'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js'
+    },
+    module: {
+        rules: [{
+            test: /\.js/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['env']
+                }
+            }
+        }]
+    },
+    devtool: 'inline-source-map'
+}
